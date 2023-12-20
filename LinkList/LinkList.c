9c7a588f526ffd6ef911944a48c66111800ce6bb
sunjichao <1549299226@qq.com>
2023-12-20 10:28:08 +0800

    [修改]完成链表的初始化API

--- LinkList/LinkList.c
+++ LinkList/LinkList.c
@@ -1,11 +1,44 @@
 #include <stdio.h>
 #include "LinkList.h"
+#include <stdlib.h>
+#include <string.h>
 
 
+enum STATUS_CODE
+{
+    ON_SUCCESS,
+    NULL_PTR,
+    MALLOC_ERROR,
+    INVALID_ACCESS,
+};
 //链表初始化
 int LinkListInit(LinkList** pList)
 {
+    int ret = 0;
+    LinkList *list = (LinkList*)malloc(sizeof(LinkList) * 1);
+    if (list == NULL)
+    {
+        return MALLOC_ERROR;
+    }
+    //清空脏数据
+    memset(list, 0, sizeof(LinkList) * 1);
 
+    list0->head = (LinkList *)malloc(sizeof(LinkList) * 1);
+    if (list->head == NULL)
+    {
+        return MALLOC_ERROR;
+    }
+    //清空脏数据
+    memset(list->head, 0, sizeof(LinkList) * 1);
+    list->head->data = 0;
+    list->head->next = NULL;
+    //链表的长度为零
+    list->len = 0;
+
+    //二级指针
+    *pList = list;
+
+    return ret;
 }
 
 //链表头插法
